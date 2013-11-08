702ac9d83ecca9ec578149027f0d97fd57df4f54
Nastyan <naastya95@gmail.com>
2013-11-08 19:14:14 +0400

    commit

---
+++ library/src/Presentlib.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @author nastyalobanova
+ * @name Presentlib
+ */
+
+function dropDownButtonActionPerformed(evt) {//GEN-FIRST:event_dropDownButtonActionPerformed
+	// TODO Добавьте свой код:
+}//GEN-LAST:event_dropDownButtonActionPerformed
---
+++ library/src/books.sql
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author nastyalobanova
+ * @name books
+ * @manual 
+ * @public
+ */ 
+Select * 
+From BOOK t1
+ Inner Join TYPEOFGENRE t on t1.GENRE = t.TYPEOFGENRE_ID
